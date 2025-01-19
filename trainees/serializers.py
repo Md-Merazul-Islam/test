b8ebaf237b5cdcf7ff19c17de3e11bf73253de6d
Md-Merazul-Islam <mdmerazul75@gmail.com>
2025-01-20 05:29:31 +0600

    refactor BookingSerializer to remove commented-out code and clean up ScheduleSerializer usage

--- trainees/serializers.py
+++ trainees/serializers.py
@@ -1,22 +1,3 @@
-# from rest_framework import serializers
-# from .models import Booking
-# from accounts.serializers import UserDetailSerializer
-
-# class BookingSerializer(serializers.ModelSerializer):
-#     trainee = UserDetailSerializer(read_only=True)
-#     # schedule = serializers.SerializerMethodField()
-
-#     class Meta:
-#         model = Booking
-#         fields = ['id', 'trainee', 'schedule', 'booking_time']
-#         read_only_fields = ['trainee', 'booking_time']
-
-#     def create(self, validated_data):
-#         request = self.context.get('request')
-#         if request and hasattr(request, 'user'):
-#             validated_data['trainee'] = request.user
-#         return super().create(validated_data)
-
 
 from rest_framework import serializers
 from .models import Booking
@@ -26,11 +7,11 @@ from admins.models import ClassSchedule
 class ScheduleSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClassSchedule
-        fields = ['date', 'start_time', 'end_time', 'current_trainees']  # Adjust fields as per your Schedule model
+        fields = ['date', 'start_time', 'end_time', 'current_trainees']  
 
 class BookingSerializer(serializers.ModelSerializer):
     trainee = UserDetailSerializer(read_only=True)
-    schedule_info = ScheduleSerializer(source='schedule', read_only=True)  # Nested serializer for GET requests
+    schedule_info = ScheduleSerializer(source='schedule', read_only=True)  
 
     class Meta:
         model = Booking
