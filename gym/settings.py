d9ee7944d17908b2126894f168239b0acfcac61d
Md-Merazul-Islam <mdmerazul75@gmail.com>
2025-01-20 11:50:36 +0600

    first commit

--- gym/settings.py
+++ gym/settings.py
@@ -118,24 +118,24 @@ WSGI_APPLICATION = 'gym.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.miwxyxgxujersguriklg',  
-        'PASSWORD': '-*_u_4JaHnkcZnH',
-        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
-        'PORT': '6543',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres.miwxyxgxujersguriklg',  
+#         'PASSWORD': '-*_u_4JaHnkcZnH',
+#         'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+#         'PORT': '6543',
+#     }
+# }
 
 
 # Password validation
